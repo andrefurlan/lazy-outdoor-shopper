1fab5a3398bc9233ac3628abcfda01e813b58603
andrefurlan <andrefurlan@gmail.com>
2014-08-18 15:16:15 -0700

    just following the tutorial

--- src/main/java/com/example/services/TimeService.java
+++ src/main/java/com/example/services/TimeService.java
@@ -1,9 +1,12 @@
 package com.example.services;
 
+import java.util.TimeZone;
+
 import com.example.models.Time;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,10 +14,14 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class TimeService {
 
-    @GET
-    public Time get() {
-        return new Time();
-    }
+	@GET
+	public Time get() {
+		return new Time();
+	}
 
+	@GET
+	@Path("/{timezone}")
+	public Time get(@PathParam("timezone") String timezone) {
+		return new Time(TimeZone.getTimeZone(timezone.toUpperCase()));
+	}
 }
-
