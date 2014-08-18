d90d4f1b940849da23979083c2f9518ad3e2d8a9
andrefurlan <andrefurlan@gmail.com>
2014-08-18 15:34:29 -0700

    agora sim

--- src/main/java/com/example/services/NameService.java
+++ src/main/java/com/example/services/NameService.java
@@ -6,9 +6,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/time")
+@Path("/name")
 @Produces(MediaType.APPLICATION_JSON)
-public class NameServie {
+public class NameService {
 
 	@GET
 	public String get() {
